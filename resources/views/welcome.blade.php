d6efb4c77b729ca9f6666fcd0663b35cefc5aea1
Pratumwan <pratumwan.bow@mail.kmutt.ac.th>
2019-03-20 15:20:20 +0700

    แก้ไฟล์ welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    โบว์มาอีกแล้วนะจ๊ะ
                 </div>
 
                 <div class="links">
