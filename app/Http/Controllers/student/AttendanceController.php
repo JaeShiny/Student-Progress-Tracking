f7969adff96244a06f738bbfd6fdcc1ad9e04040
Pratumwan <pratumwan.bow@mail.kmutt.ac.th>
2019-06-15 17:21:10 +0700

    เพิ่มหน้า student/attendance.blade.php

--- app/Http/Controllers/student/AttendanceController.php
+++ app/Http/Controllers/student/AttendanceController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Model\spts\Attendance;
 use App\Model\mis\Course;
+use App\Model\mis\Bio;
 use App\Model\spts\User;
 use Auth;
 
@@ -16,8 +17,11 @@ class AttendanceController extends Controller
     public function showAttendanceS()  {
         $student_id = Auth::user()->student_id;
         $attendance = Attendance::where('student_id',$student_id)->get();
+        $bios = Bio::where('student_id', $student_id)->get();
         return view('student.attendance',[
             'attendance' => $attendance,
+            'student_id' => $student_id,
+            'bios' => $bios,
         ]);
     }
 
--- resources/views/student/attendance.blade.php
+++ resources/views/student/attendance.blade.php
@@ -7,12 +7,56 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <title>แสดงผลการเข้าเรียน</title>
+
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li> </li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <li class="breadcrumb-item active" aria-current="page"><a href="">การเข้าเรียน</a></li>
+        </ol>
+    </nav>
+
 </head>
 <body>
-@foreach($attendance as $attendances)
-{{$attendances->course_id}} {{$attendances->amount_attendance}} <br>
-@endforeach
+        @foreach ($bios as $bio)
+        <h6 align='right'>รหัสนักศึกษา: {{$bio->student_id}} &nbsp;&nbsp;&nbsp;&nbsp;</h6>
+        <h6 align='right'>ชื่อ-สกุล: {{$bio->first_name}} &nbsp;{{$bio->last_name}}&nbsp;&nbsp;&nbsp;</h6>
+        @endforeach
+
+        <h5 align='center'>การเข้าเรียน</h5>
+
+<br><br><br>
+    <div class="container"><center>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                  <th>รหัสวิชา</th>
+                  <th>รหัสนักศึกษา</th>
+                  {{-- <th>ชื่อ-สกุล</th> --}}
+                  <th>จำนวนชั่วโมงเรียน</th>
+                  <th>จำนวนการขาดเรียน</th>
+                  <th>จำนวนการลา</th>
+                </tr>
+            </thead>
+            @foreach($attendance as $attendances)
+            <tbody>
+                <tr>
+                  <td>{{$attendances->course_id}}</td>
+                  <td>{{$attendances->student_id}}</td>
+                  {{-- <td>{{$show_student->id->name}} &nbsp;&nbsp; {{$show_student->id->last_name}}</td> --}}
+                  <td>{{$attendances->amount_attendance}}</td>
+                  <td>{{$attendances->amount_absence}}</td>
+                  <td>{{$attendances->amount_takeleave}}</td>
+                </tr>
+            </tbody>
+            @endforeach
+        </table></center>
+    </div>
+
 </body>
 </html>
 
