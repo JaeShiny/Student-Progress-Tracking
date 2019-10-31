ed8e934c53ebb856e8404a5e708c1bc357682a43
Pratumwan <pratumwan.bow@mail.kmutt.ac.th>
2019-10-31 20:05:21 +0700

    (LF) สถิติของเด็กแต่ละคน

--- resources/views/LF/studentlist.blade.php
+++ resources/views/LF/studentlist.blade.php
@@ -88,7 +88,7 @@
                 </td>
 
                 <td width="10">
-                    <a href="">
+                    <a href="/chartStudentLF/{{$student->student_id}}">
                         <center><img src="{{ URL::asset("../img/รูปสถิติ.png") }}" width="30" height="25" title="สถิติ"></center>
                     </a>
                 </td>
