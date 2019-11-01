0eda3b912baf69d542a8fbfa747f8891f4e5d46c
Pratumwan <pratumwan.bow@mail.kmutt.ac.th>
2019-11-01 16:36:03 +0700

    เอา javascript ออกจาก profile เด็ก
    เนื่องจากมันตีกับจาวาสคิปของเฮดเดอร์ ทำให้ตำแหน่งเคลื่อน

--- resources/views/student/profile(before).blade.php
+++ resources/views/student/profile(before).blade.php
@@ -6,7 +6,7 @@
 <head>
     <link rel="icon" type="img/png" href="../img/libray.png" />
     <title>ข้อมูลการสัมภาษณ์</title>
-    <link href="{{ asset('css/csste.css') }}" rel="stylesheet" type="text/css">
+    {{-- <link href="{{ asset('css/csste.css') }}" rel="stylesheet" type="text/css"> --}}
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <meta charset="utf-8">
