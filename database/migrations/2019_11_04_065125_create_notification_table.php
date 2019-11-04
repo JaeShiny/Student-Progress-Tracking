00f7fefa177ba570f69069749cadf4bab3fb8132
Pratumwan <pratumwan.bow@mail.kmutt.ac.th>
2019-11-04 14:07:30 +0700

    migrate table Notification

---
+++ database/migrations/2019_11_04_065125_create_notification_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notification', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('position');
+            $table->string('person_id');
+            $table->string('course_id');
+            $table->string('semester');
+            $table->string('year');
+            $table->json('config');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notification');
+    }
+}
